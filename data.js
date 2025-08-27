c1dbb46efba5599976c08e956404d095a0dcca0d
Willian Gattass <158415271+williangmg@users.noreply.github.com>
2025-08-26 21:40:23 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,268 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-escola-flamengo",
+      "name": "Escola flamengo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.4400334014812692,
+        "pitch": 0.0878035898846985,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8738271862606375,
+          "pitch": 0.006762666800034012,
+          "rotation": 0,
+          "target": "1-iml"
+        },
+        {
+          "yaw": 0.07200920710658387,
+          "pitch": 0.041552281013817804,
+          "rotation": 0,
+          "target": "3-politec"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5806284602631848,
+          "pitch": 0.027613202154583405,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Escola do flamengo</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">O Clube de Regatas do Flamengo disponibiliza diferentes tipos de aula,&nbsp;</font></font><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">alia o desenvolvimento de habilidades específicas&nbsp;</font></font><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">com a preparação física e psicológica dos jovens</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1-iml",
+      "name": "IML",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.6035730817810752,
+        "pitch": 0.30171361422326015,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08462689503607734,
+          "pitch": 0.025287952007445824,
+          "rotation": 0,
+          "target": "0-escola-flamengo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5012678913252309,
+          "pitch": -0.025260039660320643,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">IML</font></font>",
+          "text": "<font color=\"#e8e8e8\" face=\"Google Sans, Arial, sans-serif\"><span style=\"font-size: 16px;\"><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">O objetivo principal do Instituto Médico Legal (IML) é apoiar as investigações criminais e auxiliar na identificação de vítimas. O IML realiza exames necroscópicos, coleta de vestígios e análise de evidências, e outros.</font></font></span></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-tiradentes",
+      "name": "Tiradentes",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.566031734169206,
+        "pitch": 0.14643885294964853,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.077554792506273,
+          "pitch": -0.07509676498663964,
+          "rotation": 0,
+          "target": "3-politec"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6094312601543477,
+          "pitch": -0.0783406952298158,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Escola Tiradentes</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-politec",
+      "name": "Politec",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.2805548433981677,
+        "pitch": 0.06564481947031453,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0662144093623684,
+          "pitch": -0.015214977570950694,
+          "rotation": 0,
+          "target": "0-escola-flamengo"
+        },
+        {
+          "yaw": 0.16323068505361604,
+          "pitch": 0.03662236050342216,
+          "rotation": 0,
+          "target": "2-tiradentes"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2812751364273325,
+          "pitch": 0.007772738308151617,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Politec</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">O objetivo da POLITEC MT é desempenhar um papel crucial na investigação criminal e na elucidação de diversos tipos de casos.</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "4-escola-hlio-palma",
+      "name": "Escola Hélio Palma",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08826164506298362,
+          "pitch": 0.008405397186326269,
+          "rotation": 0,
+          "target": "1-iml"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8794686127960993,
+          "pitch": 0.025120220086950695,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Escola DR Hélio Palma</font></font>",
+          "text": "<div>Escola com acessibilidade e infraestrutura adequada para todos os estudantes, incluindo aqueles com deficiência ou mobilidade reduzida, oferecendo rampas, sinalização tátil e sonora, e banheiros adaptados. A escola conta com várias dependências, como uma biblioteca, uma quadra de esportes coberta e salas de aula climatizadas, garantindo um ambiente propício para o aprendizado.</div><div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Tour Bairro Novo MT.",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
